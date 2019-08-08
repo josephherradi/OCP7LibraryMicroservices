39d4f4b05723f409312ac93aa0c40602ef528722
Joseph Herradi <joseph.herradi@gmail.com>
2019-08-08 17:04:38 +0100

    error spring security fixed  :)

--- webservices/src/main/java/com/ocp7/webservices/Application.java
+++ webservices/src/main/java/com/ocp7/webservices/Application.java
@@ -2,8 +2,10 @@ package com.ocp7.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ocp7.webservices")
 public class Application {
 
 	public static void main(String[] args) {
--- webservices/src/main/java/com/ocp7/webservices/config/SecurityConfig.java
+++ webservices/src/main/java/com/ocp7/webservices/config/SecurityConfig.java
@@ -1,15 +1,22 @@
-package config;
+package com.ocp7.webservices.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+                .csrf().disable();
+
+
     }
 }
