5c4160ba62089c5b4532dca43e21e522ab90d8ce
Joseph Herradi <joseph.herradi@gmail.com>
2019-09-09 16:59:49 +0100

    fix basic auth

--- webservices/src/main/java/com/ocp7/webservices/config/SecurityConfig.java
+++ webservices/src/main/java/com/ocp7/webservices/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ocp7.webservices.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,8 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
 
 
     }
+
 }
